038784e36cb606248ea82cdc1e545b1b44411cd3
sylvia <limsyl@163.com>
2023-08-02 15:11:38 +0800

    4.3 add member module

---
+++ member/src/main/java/com/example/train/member/Main.java
@@ -0,0 +1,7 @@
+package com.example.train.member;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- member/src/main/java/com/example/train/member/controller/TestConntroller.java
+++ member/src/main/java/com/example/train/member/controller/TestConntroller.java
@@ -1,4 +1,4 @@
-package com.example.train;
+package com.example.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
