f7636b8abd71c9c07148097da35c7b050fd6d1a0
sylvia <limsyl@163.com>
2023-08-05 23:15:08 +0800

    4.7 增加AOP,打印请求参数和返回结果，及日志流水号

--- member/src/main/java/com/example/train/member/controller/TestController.java
+++ member/src/main/java/com/example/train/member/controller/TestController.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestConntroller {
+public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        System.out.println("hello1");
         return "hello world";
     }
 }
