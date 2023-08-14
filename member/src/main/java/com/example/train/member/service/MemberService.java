a4091c687f087d565b64f5c8e17dc920bfdaef69
sylvia <limsyl@163.com>
2023-08-14 19:28:34 +0800

    4.15 完成会员注册接口的开发

--- member/src/main/java/com/example/train/member/service/MemberService.java
+++ member/src/main/java/com/example/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.example.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.example.train.member.domain.Member;
+import com.example.train.member.domain.MemberExample;
 import com.example.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +17,19 @@ public class MemberService {
 
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);//createCriteria()用于创造条件
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if(CollUtil.isNotEmpty(list)){    //Hutool库的工具类
+            //return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());//系统时间戳
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
