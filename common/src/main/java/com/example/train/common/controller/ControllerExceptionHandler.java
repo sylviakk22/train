c71b7b682ee08f37a74e0b3299bfe3bc47db85d4
sylvia <limsyl@163.com>
2023-08-17 00:11:45 +0800

    4.19 集成校验框架Validation

--- common/src/main/java/com/example/train/common/controller/ControllerExceptionHandler.java
+++ common/src/main/java/com/example/train/common/controller/ControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.train.common.exception.BusinessException;
 import com.example.train.common.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -46,5 +47,21 @@ public class ControllerExceptionHandler {
         commonResp.setMessage(e.getE().getDesc());//getE()就是具体的异常（枚举值），getDesc()是得到枚举值的描述
         return commonResp;
     }
+    /**
+     * 校验异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = org.springframework.validation.BindException.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(BindException e)  {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("校验异常：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setSuccess(false);
+        // commonResp.setMessage("系统出现异常，请联系管理员");
+        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());//getE()就是具体的异常（枚举值），getDesc()是得到枚举值的描述
+        return commonResp;
+    }
+
 
 }
--- member/src/main/java/com/example/train/member/controller/MemberController.java
+++ member/src/main/java/com/example/train/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.example.train.common.resp.CommonResp;
 import com.example.train.member.req.MemberRegisterReq;
 import com.example.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ public class MemberController {
         return commonResp;
     }
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
 //        CommonResp<Long> commonResp = new CommonResp<>();
 //        commonResp.setContent(memberService.register(req));
 //        return commonResp;
