8ba27adbcb2cc4bd0fe72cf0dde3ca66d6804178
sylvia <limsyl@163.com>
2023-08-17 00:34:54 +0800

    4.20 使用雪花算法生成数据库id

--- member/src/main/java/com/example/train/member/service/MemberService.java
+++ member/src/main/java/com/example/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.train.member.service;
 import cn.hutool.core.collection.CollUtil;
 import com.example.train.common.exception.BusinessException;
 import com.example.train.common.exception.BusinessExceptionEnum;
+import com.example.train.common.util.SnowUtil;
 import com.example.train.member.domain.Member;
 import com.example.train.member.domain.MemberExample;
 import com.example.train.member.mapper.MemberMapper;
@@ -31,7 +32,7 @@ public class MemberService {
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
         Member member = new Member();
-        member.setId(System.currentTimeMillis());//系统时间戳
+        member.setId(SnowUtil.getSnowflakeNextId());//雪花算法
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
