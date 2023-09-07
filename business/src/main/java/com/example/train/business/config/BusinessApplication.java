66012034208450a1c9f19d493757ddd4ac1e1a86
sylvia <limsyl@163.com>
2023-09-07 15:20:04 +0800

    9.4 增加business业务模块；修改后端端口号

---
+++ business/src/main/java/com/example/train/business/config/BusinessApplication.java
@@ -0,0 +1,26 @@
+package com.example.train.business.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.example")
+@MapperScan("com.example.train.*.mapper")
+public class BusinessApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+
+    public static void main(String[] args) {
+//        SpringApplication.run(BusinessApplication.class, args);
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
+
+}
--- member/src/main/java/com/example/train/member/Main.java
+++
@@ -1,7 +0,0 @@
-package com.example.train.member;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
